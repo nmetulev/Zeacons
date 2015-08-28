a367571f288bab84da5d5442487d53872784c589
Nikola Metulev <nikolame@microsoft.com>
2015-08-27 23:35:13 -0700

    working base on rtm

--- Zeacons/MainPage.xaml.cs
+++ Zeacons/MainPage.xaml.cs
@@ -4,9 +4,11 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Bluetooth.Advertisement;
 using Windows.Storage.Streams;
 using Windows.System.Display;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
 
 namespace Zeacons
 {
@@ -15,7 +17,42 @@ namespace Zeacons
         BluetoothLEAdvertisementPublisher publisher;
         BluetoothLEAdvertisementWatcher watcher;
 
-        bool zombieEh = false;
+        private ushort COMPANY_ID = 0x0006;
+
+        private bool infected = false;
+
+        private bool Infected
+        {
+            get { return infected; }
+            set
+            {
+                if (value)
+                {
+                    watcher.Stop();
+                    publisher.Start();
+
+                    // call can come from non UI thread
+                    Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        VisualStateManager.GoToState(this, "zombie", false);
+                    });
+                }
+                else
+                {
+                    publisher.Stop();
+                    watcher.Start();
+
+                    // call can come from non UI thread
+                    Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        VisualStateManager.GoToState(this, "human", false);
+                    });
+                }
+
+                infected = value;
+            }
+        }
+
 
         public MainPage()
         {
@@ -24,105 +61,182 @@ namespace Zeacons
             // don't lock screen
             DisplayRequest request = new DisplayRequest();
             request.RequestActive();
-           
-            becomeHuman();
 
+            initWatcher();
+            initPublisher();
         }
 
-        private async void getInfected()
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-                VisualStateManager.GoToState(this, "zombie", false);
-                zombieEh = true;
-            });
+            App.Current.Suspending += App_Suspending;
+            App.Current.Resuming += App_Resuming;
 
-            if (watcher != null)
-            {
-                watcher.Stop();
-            }
-            if (publisher == null)
-            {
-                initPublisher();
-            }
+            watcher.Received += Watcher_Received;
+            watcher.Stopped += Watcher_Stopped;
 
-           
+            publisher.StatusChanged += Publisher_StatusChanged;
 
-            publisher.Start();
-
-        }
-
-        private async void becomeHuman()
-        {
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-                VisualStateManager.GoToState(this, "human", false);
-                zombieEh = false;
-
-            });
-
-            if (watcher == null)
-            {
-                initWatcher();
-            }
-
-            if (publisher != null)
-            {
-                publisher.Stop();
-            }
 
             watcher.Start();
         }
 
-        private void initPublisher()
+        private void App_Resuming(object sender, object e)
         {
-            publisher = new BluetoothLEAdvertisementPublisher();
-
-            var beaconData = new BluetoothLEManufacturerData();
-            byte[] pattern = { 0x00, 0x00, 0x00, 0x01 };
-            beaconData.Data = pattern.AsBuffer();
-            beaconData.CompanyId = 0x0006;
-
-            publisher.Advertisement.ManufacturerData.Add(beaconData);
+            watcher.Received += Watcher_Received;
+            watcher.Stopped += Watcher_Stopped;
 
             publisher.StatusChanged += Publisher_StatusChanged;
 
-            //publisher.Start();
+            Infected = Infected;
         }
+
+        private void App_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            watcher.Stop();
+            publisher.Stop();
+
+            watcher.Received -= Watcher_Received;
+            watcher.Stopped -= Watcher_Stopped;
+
+            publisher.StatusChanged -= Publisher_StatusChanged;
+        }
+
+        #region Watcher
 
         private void initWatcher()
         {
-            
+
             watcher = new BluetoothLEAdvertisementWatcher();
-            watcher.Received += Watcher_Received;
-            //watcher.Start();
+
+            // let's set up filters so we only watch for specific beacons
+            var manufacturerData = new BluetoothLEManufacturerData();
+            manufacturerData.CompanyId = COMPANY_ID;
+
+            watcher.AdvertisementFilter.Advertisement.ManufacturerData.Add(manufacturerData);
+
+            watcher.SignalStrengthFilter.InRangeThresholdInDBm = -50;
+            watcher.SignalStrengthFilter.OutOfRangeThresholdInDBm = -80;
+            watcher.SignalStrengthFilter.OutOfRangeTimeout = TimeSpan.FromMilliseconds(2000);
+
+        }
+
+        private async void Watcher_Stopped(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementWatcherStoppedEventArgs args)
+        {
+            if (args.Error != Windows.Devices.Bluetooth.BluetoothError.Success)
+            {
+                await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+                {
+                    MessageDialog dialog = new MessageDialog("Error: " + args.Error.ToString(), "Can not start Watcher");
+                    await dialog.ShowAsync();
+                });
+                
+            }
         }
 
         private void Watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
         {
-
             BluetoothLEAdvertisement advert = args.Advertisement;
 
             if (advert == null)
                 return;
 
-            foreach (var data in advert.ManufacturerData)
+            if (advert.ManufacturerData.Count > 0)
             {
-
-                if (data.CompanyId == 6 )
+                var temp = ParseAdvertisementBuffer(advert.ManufacturerData[0].Data);
+                Debug.WriteLine("received - " + "rssi: " + args.RawSignalStrengthInDBm.ToString() + " | data: " + printBytes(temp));
+                if (args.RawSignalStrengthInDBm > -50)
                 {
-                    var temp = ParseAdvertisementBuffer(data.Data);
-                    Debug.WriteLine("received - " + "rssi: " + args.RawSignalStrengthInDBm.ToString() + " | data: " + printBytes(temp));
-                    if (args.RawSignalStrengthInDBm > -50)
-                    {
-                        getInfected();
-                    }
-                    
+                    Infected = true;
                 }
-
 
             }
         }
+
+        #endregion
+
+
+        #region Publisher
+
+        private void initPublisher()
+        {
+            publisher = new BluetoothLEAdvertisementPublisher();
+
+            var manufacturerData = new BluetoothLEManufacturerData();
+            manufacturerData.CompanyId = COMPANY_ID;
+
+            var writer = new DataWriter();
+            UInt16 uuidData = 0x0001;
+            writer.WriteUInt16(uuidData);
+            manufacturerData.Data = writer.DetachBuffer();
+
+            publisher.Advertisement.ManufacturerData.Add(manufacturerData);
+
+
+        }
+
+        private async void Publisher_StatusChanged(BluetoothLEAdvertisementPublisher sender, BluetoothLEAdvertisementPublisherStatusChangedEventArgs args)
+        {
+            if (args.Error != Windows.Devices.Bluetooth.BluetoothError.Success)
+            {
+                await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+                {
+                    MessageDialog dialog = new MessageDialog("Error: " + args.Error.ToString(), "Can not start Watcher");
+                    await dialog.ShowAsync();
+                });
+
+            }
+        }
+
+        #endregion
+
+        //private async void getInfected()
+        //{
+        //    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+        //    {
+        //        VisualStateManager.GoToState(this, "zombie", false);
+        //        infected = true;
+        //    });
+
+        //    if (watcher != null)
+        //    {
+        //        watcher.Stop();
+        //    }
+        //    if (publisher == null)
+        //    {
+        //        initPublisher();
+        //    }
+
+
+
+        //    publisher.Start();
+
+        //}
+
+        //private async void becomeHuman()
+        //{
+        //    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+        //    {
+        //        VisualStateManager.GoToState(this, "human", false);
+        //        infected = false;
+
+        //    });
+
+        //    if (watcher == null)
+        //    {
+        //        initWatcher();
+        //    }
+
+        //    if (publisher != null)
+        //    {
+        //        publisher.Stop();
+        //    }
+
+        //    watcher.Start();
+        //}
+
+
+
+
 
         #region
         private byte[] ParseAdvertisementBuffer(IBuffer buffer)
@@ -161,21 +275,11 @@ namespace Zeacons
 
             if (tapped == match)
             {
-                if (zombieEh)
-                {
-                    becomeHuman();
-                }
-                else
-                {
-                    getInfected();
-                }
+                Infected = !Infected;
             }
         }
 
-        private void Publisher_StatusChanged(BluetoothLEAdvertisementPublisher sender, BluetoothLEAdvertisementPublisherStatusChangedEventArgs args)
-        {
-            Debug.WriteLine("publisher status changed - TODO handle");
-        }
+        
         #endregion
     }
 }
